ca090e605423fbef90f43e25c9fd1dc6e31a2128
Erin Van Brunt <erinmontybruce@gmail.com>
2022-04-05 13:51:05 -0700

    done

--- lib/a_type_error.rb
+++ lib/a_type_error.rb
@@ -1,3 +1,12 @@
-1 + 1
+# 1 + 1
 
-1 + "is the loneliest number"
+# 1 + "is the loneliest number"
+
+def add_up
+    arr = []
+    arr.push(1)
+    arr.push("is the loneliest number")
+    p arr
+end
+
+add_up
